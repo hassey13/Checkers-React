48e3f4f3b8614abbd17d28c7cb6e7baebd6033e2
hassey13 <ehasselbring@gmail.com>
2017-05-17 15:07:21 -0400

    added resignation

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Winner = ( props ) => {
 
-  let className = props.winner ? props.winner.toLowerCase() : 'hide'
+  let className = props.winner && props.winner !== "" ? props.winner.toLowerCase() : 'hide'
   let winner = props.winner === "BLUE" ? props.board.players[0].username : props.board.players[1].username
 
   return (
